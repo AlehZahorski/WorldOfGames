642e755a2d7c820ae917d3fdfb1d08a81a7ddba0
AlehZahorski <git3371m@gmail.com>
2021-07-06 13:47:25 +0200

    database learning started

--- app/Http/Controllers/Home/MainPage.php
+++ app/Http/Controllers/Home/MainPage.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class MainPage extends Controller
 {
     public function __invoke()
     {
+        DB::table('genres')->truncate();
+        DB::table('genres')->insert([
+            'name' => 'RPG',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
         return view('home.main');
     }
 }
